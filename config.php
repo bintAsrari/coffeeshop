458502e71379a7781cb7c25ff09c04fd9be505ee
أميرة <51089695+bintAsrari@users.noreply.github.com>
2025-02-08 22:45:43 +0800

    coffee website
    First project - coffee website
    version 1

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+//$link = mysqli_connect($db_host,$db_user,$db_pass,$db_database,$db_port);
+$conn = mysqli_connect('localhost','root','','shop_db', 3307) or die('connection failed');
+
+?>
+
+
+
